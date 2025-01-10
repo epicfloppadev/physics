de4c338ed0af0c6c23c91030ada562cc770f2445
epicfloppadev <123495508+epicfloppadev@users.noreply.github.com>
2025-01-10 19:11:44 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,4 @@
 #include "header.hpp"
-/*#include <SFML/Graphics.hpp>
-#include <vector>
-#include <iostream>
-#include <random>
-#define WINWIDTH 1000
-#define WINHEIGHT 800
-using namespace sf;
-const int gravity = 10;
-class object
-{
-public:
-    RectangleShape obj;
-    Vector2f pos;
-    float mass = 0.5;
-    bool physics = true;
-    int momentum = 0;
-    int collisioncoef = 0;
-    bool immutable;
-    object() : obj(Vector2f(100.f, 100.f)) {}
-    void create(int x, int y, Color color, bool imm);
-};
-
-void object::create(int x, int y, Color color, bool imm)
-{
-    pos = Vector2f(static_cast<float>(x), static_cast<float>(y));
-    obj.setPosition(pos);
-    obj.setFillColor(color);
-    immutable = imm;
-}
-void grav(object &o)
-{
-    o.momentum = gravity * o.mass;
-}
-std::vector<object> pool;
-
-void pushing()
-{
-    object r;
-    int xPos, yPos;
-    int getsizex = WINWIDTH - r.obj.getSize().x, getsizey = WINHEIGHT - r.obj.getSize().y;
-    xPos = rand() % getsizex;
-    yPos = rand() % getsizey;
-    int imm = false;
-    Color randomcolor = Color(rand() % 255, rand() % 255, rand() % 255);
-    r.create(xPos, yPos, randomcolor, imm);
-    pool.push_back(r);
-}
-*/
 int main()
 {
     srand(time(NULL));
@@ -81,7 +33,7 @@ int main()
             pool[i].obj.setPosition(pool[i].obj.getPosition() + Vector2f(0, float(pool[i].momentum)));
             window.draw(pool[i].obj);
 
-            std::cout<<pool[i].obj.getPosition().y<<'\n';
+            std::cout << pool[i].obj.getPosition().y << '\n';
         }
         window.display();
     }
