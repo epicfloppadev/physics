bf5b314eb278745d7607b7243eb35f2f2f2d23b3
epicfloppadev <123495508+epicfloppadev@users.noreply.github.com>
2025-01-11 14:24:17 +0200

    collision
    added small collision

--- src/header.hpp
+++ src/header.hpp
@@ -4,6 +4,7 @@
 #include <random>
 #define WINWIDTH 1000
 #define WINHEIGHT 800
+#define part 10
 using namespace sf;
 const int gravity = 10;
 class object
@@ -13,34 +14,61 @@ public:
     Vector2f pos;
     float mass = 0.5;
     bool physics = true;
-    int momentum = 0;
+    float momentumx=0,momentumy = 0;
     int collisioncoef = 0;
-    bool immutable;
+    bool immutable = false;
     object() : obj(Vector2f(100.f, 100.f)) {}
-    void create(int x, int y, Color color, bool imm);
+    void create(int x, int y, Color color, bool imm, int Xsize, int Ysize);
 };
 
-void object::create(int x, int y, Color color, bool imm)
+void object::create(int x, int y, Color color, bool imm = true, int Xsize=100, int Ysize=100)
 {
     pos = Vector2f(static_cast<float>(x), static_cast<float>(y));
+    obj.setSize({static_cast<float>(Xsize), static_cast<float>(Ysize)});
+    std::cout<<pos.y;
     obj.setPosition(pos);
     obj.setFillColor(color);
     immutable = imm;
 }
 void grav(object &o)
 {
-    o.momentum = gravity * o.mass;
+    o.momentumy = gravity * (1-o.immutable);
 }
 std::vector<object> pool;
-void pushing()
+void pushing(int Xpos = -1, int Ypos = -1, int Xsize = 100, int Ysize = 100, bool imm = false)
 {
     object r;
-    int xPos, yPos;
-    int getsizex = WINWIDTH - r.obj.getSize().x, getsizey = WINHEIGHT - r.obj.getSize().y;
-    xPos = rand() % getsizex;
-    yPos = rand() % getsizey;
-    int imm = false;
-    Color randomcolor = Color(rand() % 255, rand() % 255, rand() % 255);
-    r.create(xPos, yPos, randomcolor, imm);
+
+    int getsizex = WINWIDTH - Xsize;
+    int getsizey = WINHEIGHT - Ysize;
+
+    if (Xpos == -1 || Ypos == -1)
+    {
+        Xpos = rand() % getsizex;
+        Ypos = rand() % getsizey;
+    }
+
+    Color randomcolor(rand() % 256, rand() % 256, rand() % 256);
+
+    r.create(Xpos, Ypos, randomcolor, imm, Xsize, Ysize);
+
+
     pool.push_back(r);
+}
+
+bool colision(object &o1, object &o2)
+{
+    if(o1.obj.getGlobalBounds().findIntersection(o2.obj.getGlobalBounds())) 
+    {
+        if(o1.obj.getPosition().y<o2.obj.getPosition().y)
+        {
+            o1.obj.setPosition(Vector2f(o1.obj.getPosition().x, o2.obj.getPosition().y) + Vector2f(0,float(-o1.obj.getSize().y)));
+        }
+        else
+        {
+            o2.obj.setPosition(Vector2f(o2.obj.getPosition().x, o1.obj.getPosition().y)+ Vector2f(0,float(-o2.obj.getSize().y)));
+        }
+        return 1;
+    }
+    return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "header.hpp"
+// std::vector<int> div[part + 1][part + 1];
 int main()
 {
+    pushing(0,WINHEIGHT-1, WINWIDTH,100, 1);
+    //std::cout<<pool[0].obj.getPosition().y<<' '<<pool[0].obj.getPosition().y;
+    grav(pool[0]);
+    
     srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode({WINWIDTH, WINHEIGHT}), "Physics");
     window.setFramerateLimit(60);
@@ -12,7 +17,7 @@ int main()
         {
             for (int i = 0; i < pool.size(); i++)
             {
-                if (pool[i].obj.getPosition().y > WINHEIGHT)
+                if (pool[i].obj.getPosition().y > WINHEIGHT || pool[i].obj.getPosition().y < 0 )
                     pool.erase(pool.begin() + i);
             }
             clock.restart();
@@ -23,18 +28,31 @@ int main()
                 window.close();
             else if (event->is<Event::MouseButtonPressed>())
             {
-                pushing();
+                int Mousex,Mousey;
+                Mousex = Mouse::getPosition(window).x;
+                Mousey = Mouse::getPosition(window).y;
+                pushing(Mousex,Mousey);
                 grav(pool[pool.size() - 1]);
             }
         }
         window.clear(Color::Black);
         for (int i = 0; i < pool.size(); i++)
         {
-            pool[i].obj.setPosition(pool[i].obj.getPosition() + Vector2f(0, float(pool[i].momentum)));
-            window.draw(pool[i].obj);
-
-            std::cout << pool[i].obj.getPosition().y << '\n';
+            pool[i].obj.setPosition(pool[i].obj.getPosition() + Vector2f(0, float(pool[i].momentumy)));
+            for(int j = i + 1; j<pool.size(); j++)
+            {
+                
+                if(colision(pool[i], pool[j]) == 1)
+                { 
+                    pool[i].momentumx=pool[i].momentumy=0;
+                    pool[j].momentumx=pool[j].momentumy=0; 
+                   
+                }
+            }  
+           
+           
         }
+        for(int i = 0; i<pool.size(); i++) window.draw(pool[i].obj);
         window.display();
     }
 
