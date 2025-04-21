20e5db28cf0f0ee09f498a92523f1006d99e4522
epicfloppadev <123495508+epicfloppadev@users.noreply.github.com>
2025-04-21 19:38:57 +0300

    Update main.cpp
    sunt prost si tot nu merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,55 @@
 #include "header.hpp"
-// std::vector<int> div[part + 1][part + 1];
+
 int main()
 {
-    pushing(0,WINHEIGHT-1, WINWIDTH,100, 1);
-    //std::cout<<pool[0].obj.getPosition().y<<' '<<pool[0].obj.getPosition().y;
-    grav(pool[0]);
-    
+    sf::FloatRect inters;
+    tex1.loadFromFile("Image/red.png");
+    tex2.loadFromFile("Image/yellow.png");
+
     srand(time(NULL));
-    sf::RenderWindow window(sf::VideoMode({WINWIDTH, WINHEIGHT}), "Physics");
+    RenderWindow window(sf::VideoMode({WINWIDTH, WINHEIGHT}), "Physics");
+
+    std::vector<Object> obj; // 20 for failsafe
+    Object obt(tex1, 0, 799);
+    obt.mass = 1000000000;
+    obt.sprite.setScale({10, 8});
+    obt.immobile = true;
+    obj.push_back(obt);
     window.setFramerateLimit(60);
-    Clock clock;
-    clock.start();
     while (window.isOpen())
     {
-        if (clock.getElapsedTime() >= seconds(1))
-        {
-            for (int i = 0; i < pool.size(); i++)
-            {
-                if (pool[i].obj.getPosition().y > WINHEIGHT || pool[i].obj.getPosition().y < 0 )
-                    pool.erase(pool.begin() + i);
-            }
-            clock.restart();
-        }
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
             else if (event->is<Event::MouseButtonPressed>())
             {
-                int Mousex,Mousey;
-                Mousex = Mouse::getPosition(window).x;
-                Mousey = Mouse::getPosition(window).y;
-                pushing(Mousex,Mousey);
-                grav(pool[pool.size() - 1]);
+                Vector2i mousePos = Mouse::getPosition(window);
+                Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+                Object obt(tex1, mousePosF.x, mousePos.y);
+                obt.vx = rand() % 5 * (1 - 2 * int(rand() % 2));
+                obt.vy = rand() % 5 * (1 - 2 * int(rand() % 2));
+                obj.push_back(obt);
             }
         }
-        window.clear(Color::Black);
-        for (int i = 0; i < pool.size(); i++)
+
+        window.clear();
+        drawvec(obj, window);
+        for (int i = 0; i < obj.size(); i++)
         {
-            pool[i].obj.setPosition(pool[i].obj.getPosition() + Vector2f(0, float(pool[i].momentumy)));
-            for(int j = i + 1; j<pool.size(); j++)
+            for (int j = i + 1; j < obj.size(); j++)
             {
-                
-                if(colision(pool[i], pool[j]) == 1)
-                { 
-                    pool[i].momentumx=pool[i].momentumy=0;
-                    pool[j].momentumx=pool[j].momentumy=0; 
-                   
+                if (obj[i].con2(obj[j]))
+                {
+                    obj[i].setTexture(tex2);
+                    // aici vine formula complicata
+                    ryc(obj[i], obj[j]);
+                    rxc(obj[i], obj[j]);
+                    obj[j].setTexture(tex2);
                 }
-            }  
-           
-           
+            }
         }
-        for(int i = 0; i<pool.size(); i++) window.draw(pool[i].obj);
+        
         window.display();
     }
 
