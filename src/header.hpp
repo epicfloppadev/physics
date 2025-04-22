34f6eb1c40966e9f8f9622a3a639ca4c27bf6241
epicfloppadev <123495508+epicfloppadev@users.noreply.github.com>
2025-04-22 17:59:55 +0300

    Update header.hpp
    nus o sa reformatez codul in alta zi dupa chimie

--- src/header.hpp
+++ src/header.hpp
@@ -5,8 +5,8 @@
 #define part 5
 #define m !o1.immoble
 using namespace sf;
-const float ct = 0.8;
-const float gravity = 0.1;
+const double ct = 0.8;
+const double gravity = 0.1;
 Texture tex1("Image/red.png"), tex2("Image/yellow.png");
 sf::Color getRandomColor()
 {
@@ -19,10 +19,10 @@ class Object
 {
 public:
     Sprite sprite;
-    float mass = 1;
-    float vx = 0.1f, vy = 0.1f; // suficient pentru coliziuni
+    double mass = 1;
+    double vx = 0.1f, vy = 0.1f; // suficient pentru coliziuni
     bool immobile = false;
-    Object(const Texture &tex = tex1, float x = 0.f, float y = 0.f) : sprite(tex)
+    Object(const Texture &tex = tex1, double x = 0.f, double y = 0.f) : sprite(tex)
     {
         sprite.setPosition({x, y});
     }
@@ -52,16 +52,22 @@ void drawvec(std::vector<Object> &v, sf::RenderWindow &window)
     window.draw(v[0].sprite);
     for (auto i = 1; i < v.size(); i++)
     {
+        window.draw(v[i].sprite);
+    }
+}
+void drawvec2(std::vector<Object> &v, sf::RenderWindow &window)
+{
+    for (auto i = 1; i < v.size(); i++)
+    {
         v[i].vy += gravity;
         v[i].sprite.move({!v[i].immobile * v[i].vx, !v[i].immobile * v[i].vy});
-        window.draw(v[i].sprite);
     }
 }
 // folosit chatgpt pe ideea mea asa ca na
 void ryc(Object &o1, Object &o2)
 {
-    constexpr float ct = 0.8f;
-    constexpr float epsilon = 0.1f;
+    constexpr double ct = 0.8f;
+    constexpr double epsilon = 0.1f;
 
     const auto &rect1 = o1.sprite.getGlobalBounds();
     const auto &rect2 = o2.sprite.getGlobalBounds();
@@ -70,16 +76,16 @@ void ryc(Object &o1, Object &o2)
     if (!intersection.has_value())
         return;
 
-    const float overlapY = intersection->size.y;
+    const double overlapY = intersection->size.y;
 
-    float moveAmount = (overlapY / 2.0f) + epsilon;
+    double moveAmount = (overlapY / 2.0f);
 
     if (rect1.position.y < rect2.position.y)
     {
         if (!o1.immobile)
-            o1.sprite.move(sf::Vector2f(0.f, -moveAmount));
+            o1.sprite.move(sf::Vector2f(0.f, -moveAmount ));
         if (!o2.immobile)
-            o2.sprite.move(sf::Vector2f(0.f, moveAmount));
+            o2.sprite.move(sf::Vector2f(0.f, moveAmount ));
     }
     else
     {
@@ -89,13 +95,13 @@ void ryc(Object &o1, Object &o2)
             o2.sprite.move(sf::Vector2f(0.f, -moveAmount));
     }
 
-    const float m1 = o1.mass;
-    const float m2 = o2.mass;
-    const float v1y = o1.vy;
-    const float v2y = o2.vy;
+    const double m1 = o1.mass;
+    const double m2 = o2.mass;
+    const double v1y = o1.vy;
+    const double v2y = o2.vy;
 
-    const float new_v1y = (v1y * (m1 - m2) + 2.f * m2 * v2y) / (m1 + m2);
-    const float new_v2y = (v2y * (m2 - m1) + 2.f * m1 * v1y) / (m1 + m2);
+    const double new_v1y = -(v1y * (m1 - m2) + 2.f * m2 * v2y) / (m1 + m2) + gravity;
+    const double new_v2y = (v2y * (m2 - m1) + 2.f * m1 * v1y) / (m1 + m2) + gravity;
 
     if (!o1.immobile)
         o1.vy = new_v1y * ct;
@@ -105,17 +111,17 @@ void ryc(Object &o1, Object &o2)
 // la fel
 void rxc(Object &o1, Object &o2)
 {
-    float m1 = o1.mass, m2 = o2.mass;
-    float v1x = o1.vx, v2x = o2.vx;
+    double m1 = o1.mass, m2 = o2.mass;
+    double v1x = o1.vx, v2x = o2.vx;
 
-    float x1 = o1.sprite.getPosition().x;
-    float x2 = o2.sprite.getPosition().x;
+    double x1 = o1.sprite.getPosition().x;
+    double x2 = o2.sprite.getPosition().x;
 
     // Only respond if moving toward each other horizontally
     if ((v1x - v2x) * (x1 - x2) < 0)
     {
-        float new_v1x = (v1x * (m1 - m2) + 2 * m2 * v2x) / (m1 + m2);
-        float new_v2x = (v2x * (m2 - m1) + 2 * m1 * v1x) / (m1 + m2);
+        double new_v1x = (v1x * (m1 - m2) + 2 * m2 * v2x) / (m1 + m2);
+        double new_v2x = -(v2x * (m2 - m1) + 2 * m1 * v1x) / (m1 + m2);
 
         o1.vx = new_v1x * (1 - o1.immobile) * ct;
         o2.vx = new_v2x * (1 - o2.immobile) * ct;
